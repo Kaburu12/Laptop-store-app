ec14e38dfa4b2c713a3982715d43c0539f47bb6b
Kaburu12 <michael.kaburur@student.moringaschool.com>
2022-06-22 14:34:39 +0300

    style the background body

--- Assets/js/index.js
+++ Assets/js/index.js
@@ -2,5 +2,10 @@ document.addEventListener("DOMContentLoaded", () => {
 	
 	fetch("http://localhost:3000/laptops")
 		.then((response) => response.json())
-	.then((data)=>{console.log(data)})
+		.then((data) => {
+			console.log(data)
+			data.forEach(laptop => {
+				console.log(laptop.name)
+			});
+		})
 })
