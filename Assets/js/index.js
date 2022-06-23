5181059454fe807c8751a7feaf70fe5f07886dd1
Kaburu12 <michael.kaburur@student.moringaschool.com>
2022-06-23 13:51:28 +0300

    display laptop image and its details

--- Assets/js/index.js
+++ Assets/js/index.js
@@ -1,14 +1,21 @@
 document.addEventListener("DOMContentLoaded", () => {
-	
+fetchLaptopData()
+	like()	
+	newReviews()
+	contactAlert()
+})
+  
+// fetch laptop data function
+function fetchLaptopData() {
 	fetch("http://localhost:3000/laptops")
 		.then((response) => response.json())
-		.then((data) => {
-			console.log(data)
-			data.forEach(laptop => {
-				console.log(laptop.name)
-			});
+		.then((laptopData) => {
+			//display first laptop on screen
+			let lapName = document.querySelector("#laptop")
+			lapName.textContent = laptopData[ 0 ].name
+			let image_url = document.querySelector("#images")
+			image_url.src = laptopData[ 0 ].image_url
+			lapName.style.cursor = "pointer"
+			
 		})
-	
-
-
-})
+}
